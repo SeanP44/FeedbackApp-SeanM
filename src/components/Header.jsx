1d9586d30b376e12e33e503bfab3f0b34ba89c9c
SeanP44 <54942439+SeanP44@users.noreply.github.com>
2024-02-08 15:07:12 -0500

    Initial Commit 02/08/2024

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  }
+
+  return (
+    <header style={headerStyles}>
+      <div className='container'>
+        <Link to='/' style={{ textDecoration: 'none', color: '#FFF' }}>
+          <h2>{text}</h2>
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  text: 'Feedback UI - Built with React.JS',
+}
+
+Header.propTypes = {
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+}
+
+export default Header
